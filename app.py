b2ee4c6e5705232b4bea93e795b64249a4be5220
meu-projeto-flask <canaldemusicaaqw@gmail.com>
2026-01-13 22:17:59 -0300

    Change home route to sobre and update templatesobre

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/sobre")
+def sobre():
+return render_template("sobre.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
